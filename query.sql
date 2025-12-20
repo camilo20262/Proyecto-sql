e630ce9ebb0b94faf0239739459d6af6ceb08d88
Camilo Diaz <camilo20263@gmail.com>
2025-12-20 11:08:03 -0500

    corregir consultas

--- query.sql
+++ query.sql
@@ -1,57 +1,67 @@
 #1.¿Cuántos clientes tiene la empresa registrados actualmente?
-select  count(* )
-from customers  
+SELECT COUNT(*) AS total_clientes
+FROM customers;
 
 #2¿Qué productos vende la empresa y cuál es el precio de cada uno?
 
-select p.productName,o.quantity
-from products  as p 
-join order_details as o
-on p.productID= o.productID
-group by p.productName,o.quantity
-order by o.quantity desc
+SELECT productName, unitPrice
+FROM products;
+
 
 #3.Cuáles son las categorías de productos que maneja la empresa?
 
-select   p.productName, c.categoryName 
-from products as p
-join categories as c
-on p.categoryID = c.categoryID
+SELECT categoryID, categoryName
+FROM categories;
 
 
 #4.¿Qué empleados trabajan actualmente en la compañía y en qué país están ubicados?
-select employeeName,country
-from employees 
+SELECT employeeName, country
+FROM employees;
+
 
 #5.¿Cuántos pedidos se han realizado en total?
-select  count(orderID)
-from orders
+SELECT COUNT(*) AS total_pedidos
+FROM orders;
+
 
 #6.¿Qué empresas de transporte utiliza Northwind para enviar sus pedidos?
-select *
-from shippers 
+SELECT shipperID, companyName
+FROM shippers;
+
 
 #7.¿Qué clientes pertenecen a un país específico (por ejemplo, Alemania o Brasil)?
 
-select  contact_name,country 
-from customers 
+SELECT customer_id,
+       company_name,
+       country
+FROM customers
+WHERE country = 'Germany';
+
 
 #8.¿Qué productos están marcados como descontinuados?
-select *
-from products
-where discontinued = '0'
+SELECT productID,
+       productName,
+       discontinued
+FROM products
+WHERE discontinued = 1;
+
 
 
 
 #9¿Qué pedidos aún no han sido enviados?
-select *
-from orders
+SELECT orderID,
+       orderDate,
+       shippedDate
+FROM orders
+WHERE shippedDate IS NULL;
+
 
 #¿Qué empleados no tienen un jefe asignado?
-select  *
-from employees 
-where reportsTo =0
-
+SELECT employeeID,
+       employeeName,
+       reportsTo
+FROM employees
+WHERE reportsTo IS NULL;
 
 
 
