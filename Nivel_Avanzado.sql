036d01fe60819bfaca2e5905ec36efce146d809a
Camilo Diaz <camilo20263@gmail.com>
2025-12-28 12:52:29 -0500

    realizar consuta # 20

--- Nivel_Avanzado.sql
+++ Nivel_Avanzado.sql
@@ -9,3 +9,17 @@ LEFT JOIN order_details od
   ON p.productID = od.productID
 GROUP BY p.productName
 ORDER BY descuento_promedio DESC;
+
+
+#20. Rentabilidad por Categoría
+#Calcula el ingreso total (quantity × unitPrice × (1-discount)) por categoría de producto. ¿Cuál es la más rentable?
+
+select c.categoryName,
+		round(sum(od.quantity * od.unitPrice * (1-od.discount)),2) as ingreso_total
+fROM products p
+LEFT JOIN order_details od
+  ON p.productID = od.productID
+join categories as c
+on p.categoryID =c.categoryID
+group by 1
+order by ingreso_total desc
